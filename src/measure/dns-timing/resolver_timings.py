a3e629016df755e2f79996a2eae69a45124a70b7
Ranya Sharma <ranyasharma2004@gmail.com>
2021-06-24 03:02:08 +0000

    Committing domains and recursors-few input files

--- src/measure/dns-timing/resolver_timings.py
+++ src/measure/dns-timing/resolver_timings.py
@@ -12,7 +12,7 @@ textfile.write("status,resolver,domain,time,size\n")
 # Repeat measurements by the number defined in the loop and input data into csv, appending each time 
 k=0
 for k in range(2):
-	cmd = ["./dns-timing", "doh", "recursorsnew", "domains"]
+	cmd = ["./dns-timing", "doh", "recursors-few", "domains"]
 	output = subprocess.check_output(cmd, stderr = subprocess.STDOUT)
 	output = output.decode('unicode_escape')
 	print(output)
