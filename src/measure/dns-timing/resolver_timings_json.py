26059609962290cd2db49d31f0fd3cea7606f227
Ranya Sharma <ranyasharma2004@gmail.com>
2021-07-05 21:41:23 -0500

    Altered code to export resolver data to JSON file

---
+++ src/measure/dns-timing/resolver_timings_json.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import uuid
+import logging.config
+import collections
+import subprocess
+import logging
+import json
+log = logging.getLogger('postgres')
+all_dns_info = []
+k=0
+for k in range(100):
+	cmd = ["./dns-timing", "doh", "recursors", "domains"]
+	output = subprocess.check_output(cmd, stderr = subprocess.STDOUT)
+	output = output.decode('unicode_escape')
+	print(output)
+	try:
+		lines = output.splitlines()
+		print(lines)
+		print(len(lines))
+		for line in lines:
+			print(line)
+			status, resolver,  domain, response_time, size_or_error, datetime = line.split(',')
+			print(status, resolver, domain, response_time, size_or_error, datetime)
+			if status == "ok":
+				response_size = int(size_or_error)
+				error = None
+				all_dns_info.append({'status': status, 'resolver': resolver, 'domain': domain, 'response_time': float(response_time),'size_or_error': response_size, 'datetime': datetime})
+				print(status, resolver,  domain, response_time, response_size, datetime)
+			else:
+				response_size = None
+				error = int()
+				response_time = int()
+				print(status, resolver,  domain, response_time, error, datetime)
+				all_dns_info.append({'status': status, 'resolver': resolver, 'domain': domain, 'response_time': float(response_time),'size_or_error': error, 'datetime': datetime})
+	except Exception as e:
+		err = 'Error parsing DNS output for website {0}: {1}'
+	k = k+1
+print(all_dns_info)
+with open("data_100.json", "a") as outfile:
+        json.dump(all_dns_info, outfile)
