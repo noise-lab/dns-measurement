ffff0cb21d887a4461d1ea18cf65cb76e2ed5029
Ranya Sharma <ranyasharma2004@gmail.com>
2021-07-18 21:28:45 -0500

    Finding geographical location of each resolver

--- src/measure/dns-timing/resolver_timings_json.py
+++ src/measure/dns-timing/resolver_timings_json.py
@@ -9,7 +9,7 @@ import json
 log = logging.getLogger('postgres')
 all_dns_info = []
 k=0
-for k in range(100):
+for k in range(150):
 	cmd = ["./dns-timing", "doh", "recursors", "domains"]
 #	output = subprocess.check_output(cmd, stderr = subprocess.STDOUT)
 #	output = output.decode('unicode_escape')
@@ -24,19 +24,19 @@ for k in range(100):
 		print(len(lines))
 		for line in lines:
 			print(line)
-			status, resolver,  domain, response_time, size_or_error, datetime = line.split(',')
-			print(status, resolver, domain, response_time, size_or_error, datetime)
+			status, resolver,  domain, r_time, size_or_error, datetime = line.split(',')
+			print(status, resolver, domain, r_time, size_or_error, datetime)
 			if status == "ok":
 				response_size = int(size_or_error)
 				error = None
-				all_dns_info.append({'status': status, 'resolver': resolver, 'domain': domain, 'response_time': response_time,'size_or_error': response_size, 'datetime': datetime})
-				print(status, resolver,  domain, response_time, response_size, datetime)
+				all_dns_info.append({'status': status, 'resolver': resolver, 'domain': domain, 'r_time': r_time,'size_or_error': response_size, 'datetime': datetime})
+				print(status, resolver,  domain, r_time, response_size, datetime)
 			else:
 				response_size = None
 				error = None
-				response_time = None
-				print(status, resolver,  domain, response_time, error, datetime)
-				all_dns_info.append({'status': status, 'resolver': resolver, 'domain': domain, 'response_time': response_time,'size_or_error': error, 'datetime': datetime})
+				r_time = None
+				print(status, resolver,  domain, r_time, error, datetime)
+				all_dns_info.append({'status': status, 'resolver': resolver, 'domain': domain, 'r_time': r_time,'size_or_error': error, 'datetime': datetime})
 	except subprocess.CalledProcessError as e:
 		print(e.stdout)
 		print(e.stderr)
@@ -47,5 +47,5 @@ for k in range(100):
 		err = 'Error parsing DNS output for website {0}: {1}'
 	k = k+1
 print(all_dns_info)
-with open("data_100.json", "a") as outfile:
-        json.dump(all_dns_info, outfile)
+with open("data_150.json", "a") as outfile:
+	json.dump(all_dns_info, outfile)
