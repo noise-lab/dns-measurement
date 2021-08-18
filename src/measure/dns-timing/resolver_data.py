d51c20e431dfd668fdcf54a8513e389c4c8cfbe5
Ranya Sharma <ranyasharma2004@gmail.com>
2021-08-17 23:46:01 -0500

    50 runs

--- src/measure/dns-timing/resolver_data.py
+++ src/measure/dns-timing/resolver_data.py
@@ -10,7 +10,7 @@ from ping3 import ping
 log = logging.getLogger('postgres')
 all_dns_info = []
 k=0
-for k in range(30):
+for k in range(50):
 	cmd = ["./dns-timing", "doh", "recursors", "domains"]
 	try:
 		output = subprocess.check_output(cmd, stderr = subprocess.STDOUT)
@@ -46,5 +46,5 @@ for k in range(30):
 		print(e)
 	k = k+1
 print(all_dns_info)
-with open("data_30.json", "w") as outfile:
+with open("data_50.json", "w") as outfile:
 	json.dump(all_dns_info, outfile)
