c701dc2d903529ab56428a22c17a627aaa4608a7
Austin Hounsel <austin.hounsel@gmail.com>
2021-10-14 17:41:05 -0400

    Update script

--- src/measure/dns-timing/resolver_data_wrapper.sh
+++ src/measure/dns-timing/resolver_data_wrapper.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo python3 resolver_data.py
+echo "Resolver measurements started. Press Ctrl+C to stop."
+while true
+do
+    sudo python3 resolver_data.py
+done
+echo "Stopping resolver measurements."
